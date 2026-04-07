22be18fb305357f7afe418f5d2f36a1dad0f85a3
ayazahmedkhann02@gmail.com <ayazahmedkhann02@gmail.com>
2026-04-08 00:26:40 +0530

    new dev

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "my-demo-bucket-ayaz-123456"
-    key            = "infra/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = true
   }
 }
